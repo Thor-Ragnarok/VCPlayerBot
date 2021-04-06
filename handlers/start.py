9266457025d56e253fc1a5467b0c809e06224bbf
Softfreakz <65111266+Softfreakz@users.noreply.github.com>
2021-04-06 18:19:57 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -11,36 +11,9 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 async def start_(client: Client, message: Message):
     await message.reply_text(
         f"""<b>Hello🥳 {message.from_user.first_name}!
-I am 𝗖𝗵𝗼𝗰𝗼𝗹𝗮𝘁𝘆𝗤𝘂𝗲𝗲𝗻𝗕𝗼𝘁 VC Music Player, an close-source bot that lets you play music in your Telegram groups.
-Maintained by @TeLeTiPsOfficialOnTopicChat ❤
-For source code Join our support group @TeLeTiPsOfficialOnTopicChat.
-Use the buttons below to know more about me.
- </b>""",
-      
-       
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Command", url="https://telegra.ph/%F0%9D%97%96%F0%9D%97%B5%F0%9D%97%BC%F0%9D%97%B0%F0%9D%97%BC%F0%9D%97%B9%F0%9D%97%AE%F0%9D%98%81%F0%9D%98%86%F0%9D%97%A4%F0%9D%98%82%F0%9D%97%B2%F0%9D%97%B2%F0%9D%97%BB%F0%9D%97%95%F0%9D%97%BC%F0%9D%98%81-04-03",
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "👥 Group", url="https://t.me/TeLeTiPsOfficialOnTopicChat"
-                    ),
-                    InlineKeyboardButton(
-                        "💾 Source code", url="https://t.me/chocolatyqueenvcplayer"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "Join ChatGroup", url="https://t.me/TeLeTiPsOfficialchannel"
-                    )
-                ]
-            ]
-        )
-    )
+I am Softfreakz Music Player Bot, Created by my master Softfreakz.
+
+For any Issue, Error report or Support contact my master Softfreakz, well if you know how to contact him 😉
 
 @Client.on_message(
     filters.command("start")
